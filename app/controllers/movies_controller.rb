996a854c2fc7177368c1032124681a1e25fa34ce
NathanielMether <nmet3@student.monash.edu>
2017-10-05 10:13:23 +1100

    update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,28 @@ class MoviesController < ApplicationController
     movie_param = params[:id]
     @movie = Movie.find(movie_param)
   end
+
+  def new
+    @movie = Movie.new
+  end
+  
+  def create
+    movie_params = params.require(:movie).permit(:title, :description, :year_released, :genre)
+
+    @new_movie = Movie.new(movie_params)
+      if @new_movie.save
+        redirect_to "movies/#{new_movie.id}"
+      else
+        render :new
+      end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/movies' => 'movies#index'
+  get '/movie/new' => 'movies#new'
   get '/movies/:id' => 'movies#show'
+  post '/movies' => 'movies#create'
 end
